dfbd7f631240ac9603412d481fb0a436a6df2e62
Effan21 <maximendaa6@gmail.com>
2024-07-28 23:50:01 +0100

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,122 @@
+
+import 'package:flutter/material.dart';
+import 'package:page_transition/page_transition.dart';
+
+import 'animation/fade_animation.dart';
+import 'find_event.dart';
+
+
+void main() => runApp(
+    MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: HomePage()
+    )
+);
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> with SingleTickerProviderStateMixin{
+  AnimationController? _scaleController;
+  Animation<double>? _scaleAnimation;
+
+  bool hide = false;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    _scaleController = AnimationController(
+        vsync: this,
+        duration: Duration(milliseconds: 300)
+    );
+
+    _scaleAnimation = Tween<double>(
+        begin: 1.0,
+        end: 30.0
+    ).animate(_scaleController!)..addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        Navigator.push(context, PageTransition(child: FindEvent(), type: PageTransitionType.fade));
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+
+    _scaleController?.dispose();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage('assets/images/background.jpg'),
+                fit: BoxFit.cover
+            )
+        ),
+        child: Container(
+          padding: EdgeInsets.all(30),
+          decoration: BoxDecoration(
+              gradient: LinearGradient(
+                  begin: Alignment.bottomCenter,
+                  colors: [
+                    Colors.black.withOpacity(.9),
+                    Colors.black.withOpacity(.6),
+                    Colors.black.withOpacity(.8),
+                    Colors.black.withOpacity(.3),
+                  ]
+              )
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              const FadeAnimation(1, Text("Find the Best Locations Near you for a Good Night.",
+                style: TextStyle(color: Colors.white, fontSize: 44, fontWeight: FontWeight.w900, height: 1.1),)),
+              const SizedBox(height: 30,),
+              FadeAnimation(1.2, Text("Let us find you an event for your interest",
+                style: TextStyle(color: Colors.white.withOpacity(.8), fontSize: 25, fontWeight: FontWeight.w400),)),
+              const SizedBox(height: 150,),
+              FadeAnimation(1.4, InkWell(
+                onTap: () {
+                  setState(() {
+                    hide = true;
+                  });
+                  _scaleController?.forward();
+                },
+                child: AnimatedBuilder(
+                    animation: _scaleController!,
+                    builder: (context, child) => Transform.scale(
+                      scale: _scaleAnimation?.value,
+                      child: Container(
+                        height: 60,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(50),
+                            color: Colors.yellow[700]
+                        ),
+                        child: hide == false ? const Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: <Widget>[
+                            Text("Find nearest event", style: TextStyle(color: Colors.white, fontSize: 18, fontWeight: FontWeight.w400),),
+                            Icon(Icons.arrow_forward, color: Colors.white,)
+                          ],
+                        ) : Container(),
+                      ),
+                    )
+                ),
+              )),
+              const SizedBox(height: 60,)
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
